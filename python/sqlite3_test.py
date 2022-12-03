6905088236368c619653770228e8feac6b6d7441
Sushi <31491170+cyborgrooster@users.noreply.github.com>
2022-12-04 00:56:52 +0100

    but for you (snake eater)
    Signed-off-by: Sushi <31491170+cyborgrooster@users.noreply.github.com>

--- python/sqlite3_test.py
+++ python/sqlite3_test.py
@@ -1,6 +1,10 @@
 import sqlite3
 
-cnx = sqlite3.connect('test.db') #maak test db aan
+cnx = sqlite3.connect('python/test.db') 
+cursor = cnx.cursor() #dit ding praat basically met de db
+
+#maak test db aan
+cursor.execute("create table games (release_year integer, release_name text)")
 
 db_test_list = [ #what a thrill
     (2001, "Metal Gear Solid 2: Sons of Liberty"),
@@ -13,5 +17,18 @@ db_test_list = [ #what a thrill
     (2007, "Apollo Justice: Ace Attorney"),
     (2015, "The Great Ace Attorney: Adventures")
 ]
+
+#voer table aan db
+cursor.executemany("INSERT INTO games VALUES (?, ?)", db_test_list)
+cnx.commit()
+
+#select all rows from table
+#for row in cursor.execute("SELECT * FROM games"): #print alle rows in table games
+#    print(row)
+
+#select all rows w/ specific value
+cursor.execute("SELECT * FROM games WHERE release_year = :y", {"y": 2002})
+fetch = cursor.fetchall()
+print(fetch)
 
 cnx.close()
