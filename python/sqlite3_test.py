17895644a447a471adb2340f1ae921a37a1e11cc
Sushi <31491170+cyborgrooster@users.noreply.github.com>
2022-12-04 00:29:21 +0100

    not for honor
    Signed-off-by: Sushi <31491170+cyborgrooster@users.noreply.github.com>

--- python/sqlite3_test.py
+++ python/sqlite3_test.py
@@ -6,10 +6,12 @@ db_test_list = [ #what a thrill
     (2001, "Metal Gear Solid 2: Sons of Liberty"),
     (2004, "Metal Gear Solid 3: Snake Eater"),
     (2008, "Metal Gear Solid 4: Guns of the Patriots"),
-    (),
-    (),
-    (),
-    (),
-    (),
-    ()
+    (2002, "Pokémon Ruby"),
+    (2004, "Pokémon FireRed"),
+    (2006, "Pokémon Pearl"),
+    (2002, "Phoenix Wright: Ace Attorney - Justice for All"),
+    (2007, "Apollo Justice: Ace Attorney"),
+    (2015, "The Great Ace Attorney: Adventures")
 ]
+
+cnx.close()
