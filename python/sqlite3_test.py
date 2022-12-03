fe1834cee611ef761edd3023e8997e5ffa313d2c
Sushi <31491170+cyborgrooster@users.noreply.github.com>
2022-12-04 00:18:26 +0100

    I give my life
    Signed-off-by: Sushi <31491170+cyborgrooster@users.noreply.github.com>

---
+++ python/sqlite3_test.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+cnx = sqlite3.connect('test.db') #maak test db aan
+
+db_test_list = [ #what a thrill
+    (2001, "Metal Gear Solid 2: Sons of Liberty"),
+    (2004, "Metal Gear Solid 3: Snake Eater"),
+    (2008, "Metal Gear Solid 4: Guns of the Patriots"),
+    (),
+    (),
+    (),
+    (),
+    (),
+    ()
+]
