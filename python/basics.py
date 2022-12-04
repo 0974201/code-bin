c74d4bf4ba690c49ebcd17d295d969a1aea2e2f3
Sushi <31491170+cyborgrooster@users.noreply.github.com>
2022-12-04 03:21:05 +0100

    in my time, there'll be no one else
    Signed-off-by: Sushi <31491170+cyborgrooster@users.noreply.github.com>

--- python/basics.py
+++ python/basics.py
@@ -1,1 +1,16 @@
+#vars
+string = "dit is een string"
+int = 1
+list = ["dit is een list", "aaa", "bbb", "ccc", 1, 2, 3] # in python begint t met 0
+list2 = ["I hear it's amazing when the famous purple stuffed worm in flap-jaw space with the tuning fork does a raw blink on Hara-Kiri Rock.", "I need scissors! 61!"]
+nested_list = [list, list2] #i heard u like lists, so i put a list in your list
+tuple = ("ddd", "eee", "fff") #als een tuple eenmaal gemaakt is kunnen de waardes niet meer gewijzigd worden
+dict = {"g" : 7, "h" :  8, "i": 9} #hash-map; je kan zooi hierin bewaren key:value
+
+# print statement
 print("dis bitch empty! yeet")
+
+# formatted string - can pass along variables (\n = een enter)
+print(f'print de bovenstaande variabelen: "{string},\n{int},\n{list},\n{list2},\n{nested_list},\n{tuple},\n{dict}"')
+
+
--- python/sqlite3_test.py
+++ python/sqlite3_test.py
@@ -1,10 +1,11 @@
 import sqlite3
 
+#maak test db aan
 cnx = sqlite3.connect('python/test.db') 
 cursor = cnx.cursor() #dit ding praat basically met de db
 
-#maak test db aan
-cursor.execute("create table games (release_year integer, release_name text)")
+#maak table in test db
+#cursor.execute("create table games (release_year integer, release_name text)")
 
 db_test_list = [ #what a thrill
     (2001, "Metal Gear Solid 2: Sons of Liberty"),
@@ -14,21 +15,35 @@ db_test_list = [ #what a thrill
     (2004, "Pokémon FireRed"),
     (2006, "Pokémon Pearl"),
     (2002, "Phoenix Wright: Ace Attorney - Justice for All"),
-    (2007, "Apollo Justice: Ace Attorney"),
-    (2015, "The Great Ace Attorney: Adventures")
+    (2007, "Apollo Justice: Ace Attorney")
 ]
 
-#voer table aan db
-cursor.executemany("INSERT INTO games VALUES (?, ?)", db_test_list)
+#voer list aan db
+#cursor.executemany("INSERT INTO games VALUES (?, ?)", db_test_list) #de  vraagtekens hier maken het wat veiliger om dingen in de db te bewaren (denk aan sql injections)
+#cnx.commit() #dit is belangrijk anders stopt ie het niet in db
+
+#voeg iets toe aan db
+new_entry = [(2001, "Phoenix Wright"), (2015, "The Great Ace Attorney: Adventures"), (2022, "Pokémon Scarlet")]
+cursor.executemany("INSERT INTO games (release_year, release_name) VALUES (?,?)", new_entry)
+cnx.commit()
+
+#update row in db
+update_entry_stm = "UPDATE games SET release_name = 'Phoenix Wright: Ace Attorney' WHERE release_name = 'Phoenix Wright'"
+cursor.execute(update_entry_stm)
+cnx.commit()
+
+#delete row in db
+delete_stm = "DELETE FROM games WHERE release_name = 'Pokémon Scarlet'"
+cursor.execute(delete_stm)
 cnx.commit()
 
 #select all rows from table
-#for row in cursor.execute("SELECT * FROM games"): #print alle rows in table games
-#    print(row)
+for row in cursor.execute("SELECT * FROM games"): #print alle rows in table games
+    print(row)
 
 #select all rows w/ specific value
 cursor.execute("SELECT * FROM games WHERE release_year = :y", {"y": 2002})
-fetch = cursor.fetchall()
+fetch = cursor.fetchall() #pakt alle rows die aan query voldoen
 print(fetch)
 
 cnx.close()
