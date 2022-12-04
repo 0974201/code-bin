8a8c8a7e6f15b4bc812603a59a8856486d03fd13
Sushi <31491170+cyborgrooster@users.noreply.github.com>
2022-12-04 04:17:54 +0100

    I'm still in a dream, snake eater
    Signed-off-by: Sushi <31491170+cyborgrooster@users.noreply.github.com>

--- python/basics.py
+++ python/basics.py
@@ -7,7 +7,7 @@ int3 = 3
 int4 = 4
 
 list = ["dit is een list", "aaa", "bbb", "ccc", 1, 2, 3] # in python begint t met 0
-list2 = ["I hear it's amazing when the famous purple stuffed worm in flap-jaw space with the tuning fork does a raw blink on Hara-Kiri Rock.", "I need scissors! 61!"]
+list2 = ["I hear it's amazing when the famous purple stuffed worm in flap-jaw space with the tuning fork does a raw blink on Hara-Kiri Rock.", "I need scissors!", "61!"]
 nested_list = [list, list2] #i heard u like lists, so i put a list in your list
 leeg = []
 
@@ -20,6 +20,7 @@ print("hoi")
 
 # formatted string - can pass along variables (\n = een enter)
 print(f'print de bovenstaande variabelen: "{string},\n{int},\n{list},\n{list2},\n{nested_list},\n{tuple},\n{dict}"')
+
 
 #if statements / conditional statements: 
 if 1000 > 1:
@@ -46,3 +47,29 @@ if int4 not in list:
 #check of list leeg is
 if leeg == []:
     print("dis bitch empty! yeet")
+
+
+#loops:
+for num in range(10): #voor ieder getal tussen 0 en 10 print het nummer in de console
+    print(num)
+
+i = 0
+while i < 10: #voor zolang i kleiner is dan 10, blijf er 1 bij optellen
+    i += 1
+    print(i) #dit alles kan ook comment worden, dan blijft hij eindeloos 0 printen. niet aan te raden.
+
+#for loops kunnen ook gebruikt worden om door een lijst te gaan: 
+for item in list2:
+    print(item)
+
+for item in tuple:
+    print(item)
+
+for key in dict.keys():
+    print(key)
+
+for value in dict.values():
+    print(value)
+
+for key, value in dict.items():
+    print(f"{key} : {value}")
